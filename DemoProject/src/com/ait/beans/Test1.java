411420a9be382302566c523081faf722385b3588
admin <admin@DESKTOP-9DR9NFF>
2021-01-21 15:45:41 +0530

    branch changes

--- DemoProject/src/com/ait/beans/Test1.java
+++ DemoProject/src/com/ait/beans/Test1.java
@@ -1,7 +1,12 @@
 package com.ait.beans;
 
 public class Test1 {
-
+   public void f1() {
+	   System.out.println("F 1 functionallty");
+   }
+   public void f2() {
+	   System.out.println("F 2 functionallty");
+   }
 	public static void main(String[] args) {
 		System.out.println("Helo");
 		System.out.println("Hello world");
